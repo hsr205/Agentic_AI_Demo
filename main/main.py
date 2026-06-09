a60a7cd04cbc4400ae8b55f08e352962181ebc97
Henry Rothenberg <hsr205@aol.com>
2026-06-09 13:41:04 -0400

    Add demo logic in main.py module

--- main/main.py
+++ main/main.py
@@ -1,7 +1,17 @@
-def main() -> int:
-    print(f"Hello from {__name__}")
+from logging import Logger
 
-    return 0
+from logger.logger import AppLogger
+
+
+def main() -> int:
+    logger: Logger = AppLogger().get_logger(__name__)
+    try:
+        logger.info(f"Hello from main() method")
+        return 0
+
+    except Exception as e:
+        logger.exception(f"Exception Thrown: {e}")
+        raise Exception(e)
 
 
 if __name__ == "__main__":
