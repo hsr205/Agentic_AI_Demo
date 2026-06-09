e4e81850de397ebd7324129dd5f8981852a6d8e1
Henry Rothenberg <hsr205@aol.com>
2026-06-09 15:01:58 -0400

    Add field to config.py module

--- config/config.py
+++ config/config.py
@@ -4,7 +4,7 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
     claude_api_key: str = Field(..., description="Claude API key that allows programmatic access to various LLMs under the Anthropic umbrella")
-
+    github_access_token: str = Field(..., description="Github Access Token leveraged to programmatically accessing a Github profile")
     model_config = SettingsConfigDict(
         env_file=".env",
         env_file_encoding="utf-8",
