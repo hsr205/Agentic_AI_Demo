697678f18fb29e146571c1c724d234515726a515
Henry Rothenberg <hsr205@aol.com>
2026-06-09 16:46:13 -0400

    Add fields to config.py module

--- config/config.py
+++ config/config.py
@@ -3,8 +3,14 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
+    claude_model_name: str = Field(...,
+                                description="Claude Model to leverage during agentic AI agent application execution")
     claude_api_key: str = Field(..., description="Claude API key that allows programmatic access to various LLMs under the Anthropic umbrella")
     github_access_token: str = Field(..., description="Github Access Token leveraged to programmatically accessing a Github profile")
+
+    ticketmaster_consumer_key: str = Field(..., description="TicketMaster Consumer Key for accessing the TicketMaster platform programmatically")
+    ticketmaster_consumer_secret: str = Field(..., description="TicketMaster Consumer Secret for accessing the TicketMaster platform programmatically")
+
     model_config = SettingsConfigDict(
         env_file=".env",
         env_file_encoding="utf-8",
