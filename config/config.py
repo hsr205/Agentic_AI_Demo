b66a790ce87233fd19af22bde316fc39c944024b
Henry Rothenberg <hsr205@aol.com>
2026-06-10 10:50:04 -0400

    Add fields / refactor logic in config.py module

--- config/config.py
+++ config/config.py
@@ -4,12 +4,19 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
     claude_model_name: str = Field(...,
-                                description="Claude Model to leverage during agentic AI agent application execution")
-    claude_api_key: str = Field(..., description="Claude API key that allows programmatic access to various LLMs under the Anthropic umbrella")
-    github_access_token: str = Field(..., description="Github Access Token leveraged to programmatically accessing a Github profile")
+                                   description="Claude Model to leverage during agentic AI agent application execution")
+    claude_api_key: str = Field(...,
+                                description="Claude API key that allows programmatic access to various LLMs under the Anthropic umbrella")
+    github_access_token: str = Field(...,
+                                     description="Github Access Token leveraged to programmatically accessing a Github profile")
 
-    ticketmaster_consumer_key: str = Field(..., description="TicketMaster Consumer Key for accessing the TicketMaster platform programmatically")
-    ticketmaster_consumer_secret: str = Field(..., description="TicketMaster Consumer Secret for accessing the TicketMaster platform programmatically")
+    google_app_password: str = Field(...,
+                                     description="Google Application Password for programmatically sending emails to/from a Gmail account")
+
+    ticketmaster_consumer_key: str = Field(...,
+                                           description="TicketMaster Consumer Key for accessing the TicketMaster platform programmatically")
+    ticketmaster_consumer_secret: str = Field(...,
+                                              description="TicketMaster Consumer Secret for accessing the TicketMaster platform programmatically")
 
     model_config = SettingsConfigDict(
         env_file=".env",
