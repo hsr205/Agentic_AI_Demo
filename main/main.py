44ac2def03f81100288ebe27c1b99a7b22b8f2af
Henry Rothenberg <hsr205@aol.com>
2026-06-12 11:28:42 -0400

    Alter demo logic in main.py module

--- main/main.py
+++ main/main.py
@@ -1,15 +1,21 @@
-from client.client_orchestrator import ClientOrchestrator
+import asyncio
+
+from agent.agent_orchestrator import AgentOrchestrator
 
 
 def main() -> int:
-    user_prompt_criteria: str = (
-        "Find upcoming events for the New York Knicks at Madison Square Garden "
-        "under $50 on 2026-06-10 containing 'Watch Party' in the name."
+    user_prompt: str = (
+        """
+        Find data for Apple and Google class A stock with a start date of January 1, 2025 to December 31, 2025.
+        Then save the data to a single CSV file on my local machine at the directory: data/
+        """
     )
 
-    orchestration_engine = ClientOrchestrator()
-    status_exit_code: int = orchestration_engine.execute_pipeline(prompt=user_prompt_criteria)
-    return status_exit_code
+    app_orchestrator: AgentOrchestrator = AgentOrchestrator()
+
+    asyncio.run(app_orchestrator.run_workflow_async(user_request=user_prompt))
+
+    return 0
 
 
 if __name__ == "__main__":
